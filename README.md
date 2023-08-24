f10400ab021ca985bb056d13667e5c0dfb6c8d5b
XZXinyuZheng <xz531@georgetown.com>
2023-08-24 01:31:55 -0400

    init the repo

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # usace_scraping
+
+## Overview
+
+This project scrapes public notices in PDFs from the [US Army Corps Engineers website](https://www.usace.army.mil/) and extracts fields from unstructured PDFs into a tabular table, building a data pipeline for a dashboard managed by Atlas Public Policy to analyze the impact of oil and gas projects on wetlands 
+
+This project is underdevelopment.
+
+## Instruction
+
+The error messages and scraping results will be automatically recorded in a Google Sheet for troubleshooting. To successfully connect Python to Google, please follow the [Google API instructions](https://developers.google.com/docs/api/quickstart/python) to set up your environment and download the credentials.json. Note that Google client libraries are installed in the virtual environment. Running the codes will open up a credential page for users to log in. Please log in and authorize the connection.
+
+## Google Sheet for Trouble Shooting
+
+* Link: https://docs.google.com/spreadsheets/d/1VYcmSCuvBMiRRpUusuulIcS4kjkXrTuFZLfKOV_IxkE/edit#gid=0
---
+++ gsheet.py
@@ -0,0 +1,85 @@
+from __future__ import print_function
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+import google.auth
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# Configure the credential
+creds = None
+
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.json'):
+    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.json', 'w') as token:
+        token.write(creds.to_json())
+        
+
+def create(title):
+    """
+    Create a new google sheet table
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+        spreadsheet = {
+            'properties': {
+                'title': title
+            }
+        }
+        # Call the Sheets API
+        spreadsheet = service.spreadsheets().create(body = spreadsheet, fields = 'spreadsheetId').execute()
+        print(f"Spreadsheet ID: {(spreadsheet.get('spreadsheetId'))}")
+        return spreadsheet.get('spreadsheetId')
+    
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+    
+def update(spreadsheet_id, range_name, value_input_option, values):
+    """
+    Creates the batch_update the user has access to.
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+        """
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+        
+        body = {
+            'values': values
+        }
+        
+        result = service.spreadsheets().values().update(
+            spreadsheetId=spreadsheet_id, 
+            range=range_name,
+            valueInputOption=value_input_option, 
+            body=body).execute()
+        
+        print(f"{result.get('updatedCells')} cells updated.")
+        return result
+    
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+
